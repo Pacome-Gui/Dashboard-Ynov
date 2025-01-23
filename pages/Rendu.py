7a726d4107f691c1372f22e8e6193c794ab72332
PGN <pacome.guillermin@akuiteo.com>
2025-01-23 15:48:49 +0100

    test

--- pages/Rendu.py
+++ pages/Rendu.py
@@ -29,7 +29,7 @@ if uploaded_file is not None:
             if st.form_submit_button(label='Valider'):
                 with col2:
                     st.dataframe(edited_df[[columnX, columnY]].groupby(by=[columnX]).mean())
-                    plot = sns.barplot(edited_df[[columnX, columnY]].groupby(by=[columnX]).mean())
+                    plot = sns.barplot(edited_df[[columnX, columnY]].groupby(by=[columnX]).mean(), x=columnX, y=columnY)
                     st.pyplot(plot.figure)
     
     
