5cb6e2207f82ce84f09059c8bb282ae5ff9fecde
PGN <pacome.guillermin@akuiteo.com>
2025-01-23 14:40:12 +0100

    Prcessing 2

--- pages/Processing.py
+++ pages/Processing.py
@@ -4,5 +4,13 @@ import pandas as pd
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    
+    df  = pd.read_csv(uploaded_file, delimiter=';')
+    edited_df = st.data_editor(df)
+    
+    st.download_button(
+    label="Download data as CSV",
+    data=edited_df.to_csv(),
+    file_name="large_df.csv",
+    mime="text/csv",
+)
