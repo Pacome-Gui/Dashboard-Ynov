566d671ac87febba019d50b57c0474e248b59f30
PGN <pacome.guillermin@akuiteo.com>
2025-01-23 14:43:47 +0100

    Processing 3

--- pages/Processing.py
+++ pages/Processing.py
@@ -2,12 +2,18 @@
 import streamlit as st
 import pandas as pd
 
+#Upload
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     
-    df  = pd.read_csv(uploaded_file, delimiter=';')
+    # Read et Edition
+    df  = pd.read_csv(uploaded_file, delimiter=',')
     edited_df = st.data_editor(df)
     
+    # Multichoice select
+    selected_columns = st.multiple_choice("Sélectionner les colonnes a télécharger", df.columns)
+    
+    #Download
     st.download_button(
     label="Download data as CSV",
     data=edited_df.to_csv(),
